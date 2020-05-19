ee8c0bf0b3163a0d35f3fbf879ca7d7d455448b8
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-05-19 19:53:37 +0200

    afsdfsd

---
+++ test - copia2 - copia - copia.js
@@ -0,0 +1,67 @@
+// Life, Universe, and Everything
+// Life, Universe, and Everything
+// Life, Universe, and Everything
+// Life, Universe, and Everything
+
+class Test
+{
+	while (1 == 1)
+	{
+		
+		var i = 1;
+	}
+	
+	for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+	
+	if (2=2)
+	{	
+		if (2=2)
+		{
+			if (2=2)
+			{
+				
+							
+				if (2=2)
+				{
+					if (2=2)
+					{
+						
+						
+					}
+					
+				}
+			}
+		}
+		
+	}
+
+	if (2=2)
+	{	
+		if (2=2)
+		{
+			if (2=2)
+			{
+				
+							
+				if (2=2)
+				{
+					if (2=2)
+					{
+						
+						if (2=2)
+						{
+							
+							
+						}
+					}
+					
+				}
+			}
+		}
+		
+	}
+}
